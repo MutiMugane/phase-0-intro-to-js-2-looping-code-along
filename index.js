63e92474231ce7b38e4e0ee2519adee03b9ed277
+ <+>
2022-08-15 10:28:06 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Code your solutions in this file
+//for ([initialization]; [condition]; [iteration]) {
+   // [loop body]
+  //}
+
+//for (let age = 30; age < 40; age++) {
+  //  console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+  //}
+
+ /* const gifts = ["teddy bear", "drone", "doll"];
+
+  function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+      debugger;
+    }
+  
+    return gifts;
+  }
+  
+  wrapGifts(gifts);
+
+
+
+  function writeCards([array],eventName) {
+    for (let i = 0; i < array.length; i++) {
+
+     // console.log(`Thank you, ${Array[i]},for the wonderful ${eventName}.`);
+     return `Thank you, ${array[0]},for the wonderful ${eventName}.`;
+   //  var array = new Array();
+          //  return ('')
+
+
+            function countDown (n){
+                n=i;
+                    console.log (n);
+            }
+            countDown();
+      debugger;
+    }
+  
+    return 
+  }
+  
+  writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+*/
+const names = ["Guadalupe","Ollie","Aki"]
+  function writeCards(names){
+
+    let message = []
+    for (let i = 0; i < names.length; i++) {
+      message.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`)}
+      return message;
+
+    }
+      function countDown (){
+        countDown= 10
+        while (countDown >= 0 ){
+          console.log(countDown--);
+        }
+      }
+  
